16b15a98712e1432046c7d2d80bbcab6357e1af9
Jian Yen <jdl.yen@gmail.com>
2020-12-09 12:56:26 +1100

    add package file and references

---
+++ R/package.R
@@ -0,0 +1,19 @@
+# package file
+
+#' aae.pop.templates: parameterised population dynamics models
+#' @name aae.pop.templates
+#'
+#' @description aae.pop.templates includes population dynamics
+#'   models for a range of species to work with the \code{aae.pop}
+#'   package (described [here](https://aae-stats.github.io/aae.pop/)).
+#'
+#'   Currently includes models for \code{\link{murray_cod}} and
+#'   \code{\link{macquarie_perch}}.
+#'
+#'   See \code{\link{get_template}} and \code{\link{get_args}}
+#'   for details on defining custom population model templates.
+#'
+#' @keywords internal
+#'
+#' @docType package
+NULL
