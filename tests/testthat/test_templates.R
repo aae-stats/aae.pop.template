486a9b74cb3d234df5575dd799689187111ac6a9
Jian Yen <jdl.yen@gmail.com>
2021-03-17 11:44:05 +1100

    update tests with new template class

--- tests/testthat/test_templates.R
+++ tests/testthat/test_templates.R
@@ -4,26 +4,25 @@ test_that("murray cod template returns correct dynamics object", {
 
   # simulate from a Murray cod object
   dyn <- murray_cod()
-  arg <- get_args("murray_cod")
-  sim <- simulate(dyn, args = arg)
+  sim <- simulate(dyn)
   expect_equal(dim(sim), c(1L, 50L, 51L))
 
   # expect most processes to be defined
   expect_equal(
-    class(dyn$density_dependence_n),
+    class(dyn$dynamics$density_dependence_n),
     c("density_dependence_n", "function")
   )
   expect_equal(
-    class(dyn$covariates),
+    class(dyn$dynamics$covariates),
     c("covariates", "function")
   )
   expect_equal(
-    class(dyn$density_dependence),
+    class(dyn$dynamics$density_dependence),
     c("density_dependence", "function")
   )
-  expect_null(dyn$demographic_stochasticity)
+  expect_null(dyn$dynamics$demographic_stochasticity)
   expect_equal(
-    class(dyn$environmental_stochasticity),
+    class(dyn$dynamics$environmental_stochasticity),
     c("environmental_stochasticity", "function")
   )
 
@@ -33,26 +32,25 @@ test_that("macquarie perch template returns correct dynamics object", {
 
   # simulate from a Macquarie perch object
   dyn <- macquarie_perch()
-  arg <- get_args("macquarie_perch")
-  sim <- simulate(dyn, args = arg)
+  sim <- simulate(dyn)
   expect_equal(dim(sim), c(1L, 30L, 51L))
 
   # expect most processes to be defined
   expect_equal(
-    class(dyn$density_dependence_n),
+    class(dyn$dynamics$density_dependence_n),
     c("density_dependence_n", "function")
   )
   expect_equal(
-    class(dyn$covariates),
+    class(dyn$dynamics$covariates),
     c("covariates", "function")
   )
   expect_equal(
-    class(dyn$density_dependence),
+    class(dyn$dynamics$density_dependence),
     c("density_dependence", "function")
   )
-  expect_null(dyn$demographic_stochasticity)
+  expect_null(dyn$dynamics$demographic_stochasticity)
   expect_equal(
-    class(dyn$environmental_stochasticity),
+    class(dyn$dynamics$environmental_stochasticity),
     c("environmental_stochasticity", "function")
   )
 
@@ -62,29 +60,28 @@ test_that("platypus template returns correct dynamics object", {
 
   # simulate from a Macquarie perch object
   dyn <- platypus()
-  arg <- get_args("platypus")
-  sim <- simulate(dyn, args = arg)
+  sim <- simulate(dyn)
   expect_equal(dim(sim), c(1L, 2L, 51L))
 
   # expect most processes to be defined
   expect_equal(
-    class(dyn$density_dependence_n),
+    class(dyn$dynamics$density_dependence_n),
     c("density_dependence_n", "function")
   )
   expect_equal(
-    class(dyn$covariates),
+    class(dyn$dynamics$covariates),
     c("covariates", "function")
   )
   expect_equal(
-    class(dyn$density_dependence),
+    class(dyn$dynamics$density_dependence),
     c("density_dependence", "function")
   )
   expect_equal(
-    class(dyn$demographic_stochasticity),
+    class(dyn$dynamics$demographic_stochasticity),
     c("demographic_stochasticity", "function")
   )
   expect_equal(
-    class(dyn$environmental_stochasticity),
+    class(dyn$dynamics$environmental_stochasticity),
     c("environmental_stochasticity", "function")
   )
 
@@ -95,19 +92,19 @@ test_that("get_template returns correct template without species wrappers", {
   # check Murray cod template
   value <- get_template("murray_cod")
   target <- murray_cod()
-  target$hex <- value$hex
+  target$dynamics$hex <- value$dynamics$hex
   expect_equal(value, target)
 
   # check Macquarie perch template
   value <- get_template("macquarie_perch")
   target <- macquarie_perch()
-  target$hex <- value$hex
+  target$dynamics$hex <- value$dynamics$hex
   expect_equal(value, target)
 
   # check Murray cod template
   value <- get_template("platypus")
   target <- platypus()
-  target$hex <- value$hex
+  target$dynamics$hex <- value$dynamics$hex
   expect_equal(value, target)
 
 })
