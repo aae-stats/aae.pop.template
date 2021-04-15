adf6e85f40d05451f2a61322d40f25e799911791
Jian Yen <jdl.yen@gmail.com>
2021-04-15 11:26:19 +1000

    add ep model with basic tests

---
+++ R/estuary_perch.R
@@ -0,0 +1,237 @@
+#' @name estuary_perch
+#' @title Parameterised estuary perch population model
+#' @description Use a pre-defined population dynamics object to
+#'   simulate population dynamics of estuary perch
+#'   (*Percalates colonorum*). Model parameters are based
+#'   on existing data sets and published studies on estuary perch.
+NULL
+
+#' @rdname estuary_perch
+#'
+#' @importFrom stats rnorm
+#'
+#' @export
+#'
+#' @param k carrying capacity
+#' @param n an integer or list of integers specifying the
+#'   number of individuals removed in any given year.
+#'   If a single integer is provided,
+#'   all years are assumed to have the same number of
+#'   additions or removals. If a list is provided,
+#'   it must have one value for each year. Defaults to
+#'   \code{0}, which will specify no additions
+#' @param ntime number of time steps used in population
+#'   simulation. Defaults to \code{50} and is required
+#'   to ensure simulated additions or removals are defined
+#'   for every simulated time step
+#' @param start time step at which additions or removals start
+#'   if \code{n} is an integer. Defaults to
+#'   \code{1}
+#' @param end time step at which additions or removals finish
+#'   if \code{n} is an integer. Defaults to
+#'   \code{1}
+#'
+#' @details The \code{estuary_perch} population model is a
+#'   stage-structured model with 2 classes and includes negative
+#'   density dependence, environmental and demographic
+#'   stochasticity, and optional associations with hydrological
+#'   conditions, instream and riparian habitat, and predators.
+#'
+#'   The estuary perch population template requires
+#'   several additional arguments and allows several optional
+#'   arguments, described individually above.
+#'
+#' @examples
+#' # define a basic model for estuary perch
+#' p <- estuary_perch()
+#'
+#' # simulate from this model
+#' sims <- simulate(p, nsim = 100)
+#'
+#' # plot the simulated values
+#' plot(sims)
+estuary_perch <- function(k = 30000, n = 0, ntime = 50, start = 1, end = 1) {
+  get_template(
+    sp = "estuary_perch",
+    k = k,
+    n = n,
+    ntime = ntime,
+    start = start,
+    end = end
+  )
+}
+
+# internal function: define species defaults
+template_estuary_perch <- function(k = 30000, n = 0, ntime = 50, start = 1, end = 1) {
+
+  # how many stages are we going to work with?
+  nstage <- 40
+
+  # and which are reproductive?
+  reproductive <- 3:40
+
+  # define  a survival function
+  survival_gen <- function(mat, ...) {
+    plogis(rnorm(length(mat), mean = qlogis(mat), sd = 0.5 * abs(qlogis(mat))))
+  }
+
+  # define a reproduction function
+  reproduction_gen <- function(
+    mat,
+    proportion_female = 0.5,
+    proportion_breeding = 0.85,
+    ...
+  ) {
+
+    # simulate realised fecundity in a given year
+    n_offspring <- rpois(1, lambda = mat)
+
+    # estimate and return reproduction estimates
+    n_offspring * proportion_female * proportion_breeding
+
+  }
+
+  # define base matrix
+  popmat <- matrix(0, nrow = nstage, ncol = nstage)
+
+  # surivval of egg, larvae and 0+ fish
+  early_survival <- c(0.5, 0.0061, 0.1226)
+
+  # add survival
+  age_frequency <- (1020.23 * (seq_len(nstage) ^ -1.16)) - 13.5
+  popmat[transition(popmat)] <- age_frequency[-1] / age_frequency[-length(age_frequency)]
+
+  # add reproduction
+  fecundity_by_age <- exp(-11.83 + 4.21 * log(384.69 * (1 - exp(-0.19 * (reproductive + 2.27)))))
+  popmat[reproduction(popmat, dims = reproductive)] <- prod(early_survival) * fecundity_by_age
+
+  # define custom density dependence function
+  k_female <- k * 0.5     # assumes 50:50 F:M sex ratio in adults
+  theta_ricker <- function(x, n, theta = 4, r = 0.4) {
+    x * exp(r * (1 - (n[2] / k_female) ^ theta)) / exp(r)
+  }
+  dens_depend <- density_dependence(
+    reproduction(popmat),
+    theta_ricker
+  )
+
+  # covariate effects based on probability of spawning and larval survival
+  #   due to flow conditions prior to and following spawning
+  # TODO: explore ways to calculate this internally, would apply to mac perch temp
+  #   effects as well
+  fecundity_effects <- function(
+    mat,
+    x,
+    ...
+  ) {
+
+    # return directly scaled value because x is calculated as a probability
+    mat * x
+
+  }
+
+  # combine into a covariates object
+  covars <- covariates(
+    masks = reproduction(popmat, dims = reproductive),
+    funs = fecundity_effects
+  )
+
+  # define environmental stochasticity
+  envstoch <- environmental_stochasticity(
+    masks = list(
+      masks = combine(survival(popmat, dims = 2), transition(popmat, dims = 1)),
+      reproduction(popmat)
+    ),
+    funs = list(
+      survival_gen,
+      reproduction_gen
+    )
+  )
+
+  # use density_dependence_n to include translocations
+  dd_n_masks <- list(
+    all_classes(popmat, dims = 2)
+  )
+  dd_n_fns <- list(
+    add_remove
+  )
+  dens_depend_n <- density_dependence_n(
+    masks = dd_n_masks,
+    funs = dd_n_fns
+  )
+
+  # collect arguments for species if required
+  arguments <- get_args(
+    "estuary_perch",
+    n = n,
+    ntime = ntime,
+    start = start,
+    end = end
+  )
+
+  # return template
+  list(
+    dynamics = list(
+      matrix = popmat,
+      covariates = covars,
+      environmental_stochasticity = envstoch,
+      density_dependence = dens_depend,
+      density_dependence_n = dens_depend_n
+    ),
+    arguments = arguments
+  )
+
+}
+
+#' @rdname estuary_perch
+#'
+#' @importFrom stats pnorm rnorm runif
+#'
+#' @export
+#'
+args_estuary_perch <- function(n = 0, ntime = 50, start = 1, end = 1) {
+
+  # helper to define removals process
+  define_translocation <- function(start, end, n) {
+
+    # set up a sequence of iterations at which individuals are removed
+    if (length(n) == 1) {
+      x <- rep(0, ntime)
+      x[start:end] <- n
+      n <- x
+    } else {
+      if (length(n) != ntime) {
+        stop("if n is a vector it must have one value for each time step",
+             call. = FALSE)
+      }
+    }
+
+    # define this as a function
+    translocate <- function(obj, pop, iter) {
+
+      # return
+      list(
+        n = n[iter], add = TRUE
+      )
+
+    }
+
+    # return
+    translocate
+
+  }
+
+
+  # return named list of args
+  list(
+
+    # to define additions through translocation
+    density_dependence_n = list(
+      define_translocation(
+        start = start, end = end, n = n
+      )
+    )
+
+  )
+
+}
